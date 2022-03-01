a214ffe0c6dfa903379b42fe55b3c89d574f6896
Dany Varghese <gagiprha@gmail.com>
2022-03-01 11:57:30 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='PyILP',
+    url='https://github.com/danyvarghese/PyILP.git',
+    author='Dany Varghese',
+    author_email='dany.incito@gmail.com',
+    # Needed to actually package something
+    packages=['PyILP'],
+    # Needed for dependencies
+    install_requires=['numpy','pyswip'],
+    # *strongly* suggested for sharing
+    version='0.0.1',
+    # The license can be anything you like
+    license='unlicense',
+    zip_safe=False,
+)
